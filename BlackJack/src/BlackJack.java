b622959c4fa82941bc22b4d1c7e1d2d5ca239bf7
Andistar12 <nguyenandy41@gmail.com>
2018-01-19 20:15:28 -0800

    Add to repository

---
+++ BlackJack/src/BlackJack.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Andy Nguyen on 1/19/2018.
+ * Where the game is played. Includes the main(String[]) method
+ */
+public class BlackJack {
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+
+        Player player = new Player("USER");
+        Dealer dealer = new Dealer();
+
+        Deck deck = new Deck();
+    }
+}
